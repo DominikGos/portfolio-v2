3de60a5bcce53ea41b7230cda0a23bab213e7c09
DominikGos <dominikgos3@gmail.com>
2025-04-29 14:23:18 +0200

    Added desktop view for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,14 +9,19 @@ export default function Home() {
     ];
 
     return (
-        <Layout className="bg-linear-to-r from-slate-800 to-slate-900  text-white">
-            <div className="flex flex-col items-center justify-center p-2 w-full gap-4">
-                <div className="w-full">
-                    <h1 className="text-5xl font-bold">Dominik Gos</h1>
-                    <p className="text-xl font-bold">Full stack developer</p>
+        <Layout className="bg-linear-to-r from-slate-800 to-slate-900  text-white p-2">
+            <div className="flex flex-col items-center justify-center p-2 w-full gap-4 md:flex-row md:p-4 lg:p-8 xl:p-12 2xl:ps-16 2xl:pe-16">
+                <div className="w-full flex flex-col gap-2">
+                    <h1 className="text-5xl font-bold xl:text-7xl">Dominik Gos</h1>
+                    <p className="text-xl font-bold text-gray-200 xl:text-2xl">Full stack developer</p>
+                    <div className="flex flex-col items-start w-full">
+                        <MainNavbar className="w-full mt-4 mb-3" items={navbarItems} />
+                    </div>
                 </div>
-                <div className="flex flex-col items-start w-full">
-                    <MainNavbar className="w-full mt-2" items={navbarItems} />
+                <div>
+                    <p className="text-gray-200">
+                        Hi, I am Dominik 👋. I am currently studying computer science at the Kielce University of Technology. I am looking for a job as a full-stack developer.
+                    </p>
                 </div>
             </div>
         </Layout>
